752addff0d4eab4df4fa1bc3692393293f8a36ad
Matthew <matthewajustice@gmail.com>
2024-06-14 18:01:36 -0400

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  important: true,
+  blocklist: ['container'],
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
+	theme: {
+		extend: {
+      margin: {
+        section: 'var(--space-section)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+        barlow: ['Barlow', ...defaultTheme.fontFamily.display],
+      },
+      fontSize: {
+        min: 'var(--step-00)',
+        bread: 'var(--step-0)',
+        subtitle: 'var(--step-1)',
+        title: 'var(--step-5)',
+        headline: 'var(--step-6)',
+      },
+      colors: {
+        light: '#F8FAFC',
+        dark: '#0D111C',
+      },
+    },
+	},
+	plugins: [],
+}
