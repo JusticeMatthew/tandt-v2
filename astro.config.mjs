88c66e227e7c369c844a632406908ab6c7d0fa8b
Matthew <matthewajustice@gmail.com>
2024-06-15 18:02:59 -0400

    testing prerendering srategies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import lenis from "astro-lenis";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lenis()]
+  integrations: [tailwind(), lenis()],
+  output: "hybrid",
+  adapter: vercel()
 });
