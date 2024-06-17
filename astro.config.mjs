c574db3a485b9c88a4b8c9634d517b9a490fc479
Matthew <matthewajustice@gmail.com>
2024-06-17 11:05:12 -0400

    removed cannonical url for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tandt-v2.vercel.app/',
   redirects: {
     '/gallery': '/#gallery'
   },
