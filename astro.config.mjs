4cf1f565db60526a440f905dd69f3658fb9fb7f3
Matthew <matthewajustice@gmail.com>
2024-06-16 21:02:47 -0400

    adjusted hover animations and testing no lenis again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import lenis from "astro-lenis";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tandt-v2.vercel.app/',
-  integrations: [tailwind(), lenis()],
+  integrations: [tailwind()],
 });
