1e2d8525a5733aa598f8ebefd91f4cc814659955
Matthew <matthewajustice@gmail.com>
2024-06-16 20:39:44 -0400

    added canonical url and readded lenis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import lenis from "astro-lenis";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://tandt-v2.vercel.app/',
+  integrations: [tailwind(), lenis()],
 });
